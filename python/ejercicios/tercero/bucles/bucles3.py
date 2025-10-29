38af2cc8829d14f3a7012725dea5f89bad48358b
Jorge Niebla Núñez <jorniebla+github@gmail.com>
2025-10-29 13:21:09 +0000

    Cambios en ejercicios de bucles y nuevo ejercicio de funciones 3º

--- python/ejercicios/tercero/bucles/bucles3.py
+++ python/ejercicios/tercero/bucles/bucles3.py
@@ -3,4 +3,5 @@
 # Crea un programa que pida al usuario introducir
 # una contraseña hasta que introduzca la correcta.
 # La contraseñar será: "PYTHON"
+# Pista usa un while y un input dentro del while
 # -----------------------------------------------
--- python/ejercicios/tercero/bucles/bucles4.py
+++ python/ejercicios/tercero/bucles/bucles4.py
@@ -1,5 +1,12 @@
 # Bucles - 3º ESO - Ejercicio 4
 # -----------------------------------------------
-# Crea un programa que pida al usuario introducir
-# un número y haga una cuenta atrás hasta 0
+# Crea un programa que recorra la lista alumnos
+# y diga si están aprobados o no.
+# ¡Ojo! la lista de alumnos está compuestas por 
+# listas más pequeñas que representan nombre y nota
+# Pista, recuerda que usas corchetes para acceder
+# a índices.
 # -----------------------------------------------
+
+lista_alumnos = [ ["Jesús", 7.2], ["Alberto", 3.5], ["Sandra", 9.0], ["Ramón", 10], ["Lucía", 2.1], ["Fabiola", 6.2], ["Diana", 5.0] ]
+
