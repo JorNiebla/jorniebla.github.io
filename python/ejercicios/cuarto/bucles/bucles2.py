815a04c18c28a513b0326a8f9a6c0efa7d363adf
Jorge Niebla Núñez <jorniebla+github@gmail.com>
2025-11-21 15:01:52 +0000

    Errata

--- python/ejercicios/cuarto/bucles/bucles2.py
+++ python/ejercicios/cuarto/bucles/bucles2.py
@@ -1,5 +1,5 @@
 # Bucles - 4º ESO - Ejercicio 2
 # -----------------------------------------------
-# Crea que pida al usuario un número entero e 
-# imprima los números pares menores que ese.
+# Crea un programa que pida al usuario un número 
+# entero e imprima los números pares menores que ese.
 # -----------------------------------------------
