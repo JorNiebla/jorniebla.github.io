37d620913966cc7d457df2d12b8e64f11f0408a1
Jorge Niebla Núñez <jorniebla+github@gmail.com>
2025-11-21 11:23:48 +0000

    Secuencias 4

--- python/ejercicios/cuarto/secuencias/secuencias4.py
+++ python/ejercicios/cuarto/secuencias/secuencias4.py
@@ -1,6 +1,10 @@
-def greet(name):
-    print ('Hello', name)
-
-greet('Jackiwe4')
-greet('Jill')
-greet('Bob')
+# Secuencias - 4º ESO - Ejercicio 4
+# -----------------------------------------------
+# Escribe una programa que pida al usuario introducir
+# su edad y le imprima su equivalente en meses y en días.
+#
+# Para hacer más sencillo, no vamos a tener en cuenta
+# los años bisiestos ni los meses extras, es decir,
+# si alguien tiene 13 años y 4 meses de edad el 
+# calculo se hace solo teniendo en cuenta los 13 años.
+# -----------------------------------------------
